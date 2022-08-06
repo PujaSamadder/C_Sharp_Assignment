9b1a62c6ab3b00325338109928046d36455b7f0a
Puja Samdder <pujasamadder05@gmail.com>
2022-08-06 21:52:27 +0530

    second commit

---
+++ ConsoleApp5/areaANDcircumference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace areaANDcircumference
+{
+     class Program
+    {
+        double r;
+
+        public void Circle()
+        {
+            Console.WriteLine("Enter Radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Area of Circle {0} is {1}", r, 3.14 * r * r);
+            Console.WriteLine("Circumference of Circle {0} is {1}", r, 2 * 3.14 * r);
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program  obj = new Program();
+                obj.Circle();
+                Console.ReadLine();
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Some error occurred");
+            }
+        }
+    }
+}
